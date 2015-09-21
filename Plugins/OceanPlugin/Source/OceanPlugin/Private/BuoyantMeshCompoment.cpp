ebd78cfae96498424fbfd1d34f380729e085d663
Enrico Donnici <enricodonnici@gmail.com>
2015-09-21 13:03:49 +0200

    Added OceanProject headers.

--- Plugins/OceanPlugin/Source/OceanPlugin/Private/BuoyantMeshCompoment.cpp
+++ Plugins/OceanPlugin/Source/OceanPlugin/Private/BuoyantMeshCompoment.cpp
@@ -1,6 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*=================================================
+* FileName: BuoyantMeshComponent.cpp
+*
+* Created by: quantumv
+* Project name: OceanProject
+* Unreal Engine version: 4.9
+* Created on: 2015/09/21
+*
+* Last Edited on: 2015/09/21
+* Last Edited by: quantumv
+*
+* -------------------------------------------------
+* For parts referencing UE4 code, the following copyright applies:
+* Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+*
+* Feel free to use this software in any commercial/free game.
+* Selling this as a plugin/item, in whole or part, is not allowed.
+* See "OceanProject\License.md" for full licensing details.
+* =================================================*/
 
-#include "BandK.h"
+#include "OceanPluginPrivatePCH.h"
 #include "BuoyantMeshCompoment.h"
 
 #include "PhysicsEngine/BodySetup.h"
