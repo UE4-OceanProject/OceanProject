00be6d85058adb9fd42e9713519d6d78a5d514c2
johnfg10 <j.gray26@hotmail.com>
2017-07-13 19:44:49 +0100

    4.16 compatibility

--- Source/OceanProject.Target.cs
+++ Source/OceanProject.Target.cs
@@ -8,18 +8,6 @@ public class OceanProjectTarget : TargetRules
 	public OceanProjectTarget(TargetInfo Target)
 	{
 		Type = TargetType.Game;
-	}
-
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "OceanProject" } );
-	}
+        ExtraModuleNames.AddRange(new string[] { "OceanProject" });
+    }
 }
