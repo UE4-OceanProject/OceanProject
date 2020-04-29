1936e961e106bb4f5541210382693db5cb81fe07
Sascha Elble <sascha.elble@gmail.com>
2020-04-28 23:44:46 -0500

    Commit before removing moving ocean to its own plugin

---
+++ CleanProjectAndPlugins.bat
@@ -0,0 +1,20 @@
+rmdir /q/s "Binaries"
+rmdir /q/s "Intermediate"
+
+rmdir /q/s "Plugins/TimePlugin/Binaries"
+rmdir /q/s "Plugins/TimePlugin/Intermediate"
+
+rmdir /q/s "Plugins/SkyPlugin/Binaries"
+rmdir /q/s "Plugins/SkyPlugin/Intermediate"
+
+rmdir /q/s "Plugins/OceanShaderPlugin/Binaries"
+rmdir /q/s "Plugins/OceanShaderPlugin/Intermediate"
+
+rmdir /q/s "Plugins/OceanPlugin/Binaries"
+rmdir /q/s "Plugins/OceanPlugin/Intermediate"
+
+rmdir /q/s "Plugins/BuoyancyPlugin/Binaries"
+rmdir /q/s "Plugins/BuoyancyPlugin/Intermediate"
+
+rmdir /q/s "Plugins/FishPlugin/Binaries"
+rmdir /q/s "Plugins/FishPlugin/Intermediate"
